761ca14a507133362668a4aa545ca919a2c7a328
GordionA1 <farisazka2506@gmail.com>
2026-04-30 20:11:48 +0700

    menyelesaikan step step implementasi fungsi selanjutnya

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -52,5 +52,30 @@ public:
             START = newNode;
             return;
         }
+            //   insert in between node
+        // Step 8: Locate position for insertion
+        Node *current = START;
+        while (current->next != NULL && current->next->noMhs < nim)
+        {
+            current = current->next;
+        }
+
+        if (current->next != NULL && nim == current->next->noMhs)
+        {
+            cout << "\nDuplicate roll numbers not allowed" << endl;
+            return;
+        }
+
+        // Step 9: Insert between current and current->next
+        newNode->next = current->next; // Step 9a
+        newNode->prev = current;       // Step 9b
+
+        //  insert last node
+        if (current->next != NULL)
+            current->next->prev = newNode;  //  Step 9c
+
+        current->next = newNode;            //  Step 9d
     }
+
 };
+
